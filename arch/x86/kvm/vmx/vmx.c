e2b7410ee6ab38c16ec298e193cf31e1ab61dc68
Isaku Yamahata <isaku.yamahata@intel.com>
2022-04-22 16:56:51 -0700

    KVM: Refactor CPU compatibility check on module initialiization
    Although non-x86 arch doesn't break as long as I inspected code, it's by
    code inspection.  This should be reviewed by each arch maintainers.
    
    kvm_init() checks CPU compatibility by calling
    kvm_arch_check_processor_compat() on all online CPUs.  Move the callback
    to hardware_enable_nolock() and add hardware_enable_all() and
    hardware_disable_all().
    Add arch specific callback kvm_arch_post_hardware_enable_setup() for arch
    to do arch specific initialization that required hardware_enable_all().
    This makes a room for TDX module to initialize on kvm module loading.  TDX
    module requires all online cpu to enable VMX by VMXON.
    
    If kvm_arch_hardware_enable/disable() depend on (*) part, such dependency
    must be called before kvm_init().  In fact kvm_intel() does.  Although
    other arch doesn't as long as I checked as follows, it should be reviewed
    by each arch maintainers.
    
    Before this patch:
    - Arch module initialization
      - kvm_init()
        - kvm_arch_init()
        - kvm_arch_check_processor_compat() on each CPUs
      - post arch specific initialization ---- (*)
    
    - when creating/deleting first/last VM
       - kvm_arch_hardware_enable() on each CPUs --- (A)
       - kvm_arch_hardware_disable() on each CPUs --- (B)
    
    After this patch:
    - Arch module initialization
      - kvm_init()
        - kvm_arch_init()
        - kvm_arch_hardware_enable() on each CPUs  (A)
        - kvm_arch_check_processor_compat() on each CPUs
        - kvm_arch_hardware_disable() on each CPUs (B)
      - post arch specific initialization  --- (*)
    
    Code inspection result:
    (A)/(B) can depend on (*) before this patch.  If there is dependency, such
    initialization must be moved before kvm_init() with this patch.  VMX does
    in fact.  As long as I inspected other archs and find only mips has it.
    
    - arch/mips/kvm/mips.c
      module init function, kvm_mips_init(), does some initialization after
      kvm_init().  Compile test only.  Needs review.
    
    - arch/x86/kvm/x86.c
      - uses vm_list which is statically initialized.
      - static_call(kvm_x86_hardware_enable)();
        - SVM: (*) is empty.
        - VMX: needs fix
    
    - arch/powerpc/kvm/powerpc.c
      kvm_arch_hardware_enable/disable() are nop
    
    - arch/s390/kvm/kvm-s390.c
      kvm_arch_hardware_enable/disable() are nop
    
    - arch/arm64/kvm/arm.c
      module init function, arm_init(), calls only kvm_init().
      (*) is empty
    
    - arch/riscv/kvm/main.c
      module init function, riscv_kvm_init(), calls only kvm_init().
      (*) is empty
    
    Co-developed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -8397,6 +8397,15 @@ static void vmx_exit(void)
 }
 module_exit(vmx_exit);
 
+/* initialize before kvm_init() so that hardware_enable/disable() can work. */
+static void __init vmx_init_early(void)
+{
+	int cpu;
+
+	for_each_possible_cpu(cpu)
+		INIT_LIST_HEAD(&per_cpu(loaded_vmcss_on_cpu, cpu));
+}
+
 static int __init vmx_init(void)
 {
 	int r, cpu;
@@ -8434,6 +8443,7 @@ static int __init vmx_init(void)
 	}
 #endif
 
+	vmx_init_early();
 	r = kvm_init(&vmx_init_ops, sizeof(struct vcpu_vmx),
 		     __alignof__(struct vcpu_vmx), THIS_MODULE);
 	if (r)
@@ -8453,12 +8463,8 @@ static int __init vmx_init(void)
 	}
 
 	vmx_setup_fb_clear_ctrl();
-
-	for_each_possible_cpu(cpu) {
-		INIT_LIST_HEAD(&per_cpu(loaded_vmcss_on_cpu, cpu));
-
+	for_each_possible_cpu(cpu)
 		pi_init_cpu(cpu);
-	}
 
 #ifdef CONFIG_KEXEC_CORE
 	rcu_assign_pointer(crash_vmclear_loaded_vmcss,
