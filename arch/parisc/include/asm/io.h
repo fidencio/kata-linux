a545bc6af4398f94578c3d1ec12cacfabdc84b27
Elena Reshetova <elena.reshetova@intel.com>
2022-11-09 10:12:42 +0200

    asm/io.h: Add ioremap_driver_hardening_uc/wc/np
    These functions is for declaring memory that should be shared with
    a hypervisor in a confidential guest. If the architecture doesn't
    implement it, it's just ioremap_uc/wc/np.
    
    Note these functions are not supposed to be used directly by drivers.
    But it will be used implicitly by devm_ioremap.
    
    Signed-off-by: Elena Reshetova <elena.reshetova@intel.com>

--- arch/parisc/include/asm/io.h
+++ arch/parisc/include/asm/io.h
@@ -133,6 +133,8 @@ void __iomem *ioremap(unsigned long offset, unsigned long size);
 #define ioremap_driver_hardened		ioremap
 /* Share memory with host in confidential guest platforms (WB version) */
 #define ioremap_cache_shared		ioremap
+#define ioremap_driver_hardened_uc	ioremap_uc
+#define ioremap_driver_hardened_wc	ioremap_wc
 #define ioremap_uc			ioremap
 
 extern void iounmap(const volatile void __iomem *addr);
